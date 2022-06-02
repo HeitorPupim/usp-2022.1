9670e516b2b0030b033476b29ac32aea228e1df6
HeitorPupim <heitorpupimtoledo@gmail.com>
2022-06-02 14:05:26 -0300

    void

---
+++ alg-II/Exerc 3 - BTree/in_out/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 02/06/2022 13:39:21 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Exercício 3 - B-Tree (Peso 3x na nota)
---
+++ alg-II/Exerc 3 - BTree/source/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+    Pra registro - utilizar as funções no trab de indices primarios
+
+    Arvore:
+
+    consulta a chave primaria antes de inserir p/ n inserir duplicadas
+    
+    novas chaves -> insere -> nó folha 
+        se superar limite de chaves -> balanceamento -> divisão/promoção do nó
+
+    estrutura da arvore:
+        Header: RRN raiz / Espaço livre
+        Nós: Chave primaria/ RRN registro / Filho/ nChave/ flag éFolha/ espaço livre
+        
+    obs:
+
+    1. Cada nó, quando carregado na memória, pode ser armazenado em uma TAD.
+    2. 
+    utilizar 4096 bytes no arquivo
+*/
+
+
+    return 0;
+}
