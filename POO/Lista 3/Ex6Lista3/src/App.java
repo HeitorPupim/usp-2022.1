c4c420f84b190aefa3ab13efcfbdcbc28a6f94aa
HeitorPupim <heitorpupimtoledo@gmail.com>
2022-06-07 00:08:58 -0300

    Ex6 Feito

--- POO/Lista 3/Ex6Lista3/src/App.java
+++ POO/Lista 3/Ex6Lista3/src/App.java
@@ -1,31 +1,55 @@
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Random;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Path filePath = Path.of("/home/heitor/workspace/usp-2022.1/POO/Lista 3/Ex6Lista3/text.txt");
-        
+        //lê o arquivo de texto "text.txt" do diretório atual.
+        Path filePath = Path.of(System.getProperty("user.dir") + "/text.txt");
+        // lê o conteúdo e armazena na string content.
         String content = Files.readString(filePath);
+        // transforma a string content, substituindo "muito" por "pouco".
+        content = content.replaceAll("muito", "pouco");
+
+        //verificiando se deu certo de trocar a string.
+        //System.out.println(content);
 
 
+        //Gerando novo arquivo: 
+
+        //Agora vamos gerar um novo arquivo. -> RandomAcessFile.
+        String diretorioSaida = System.getProperty("user.dir") + "/saida.txt";
+        //File fTemp = new File(diretorioSaida);
+
+        //modo ler e escrever.
+        RandomAccessFile raf = new RandomAccessFile(diretorioSaida, "rw");
+        //escrevendo no arquivo a partir do início.
+        raf.seek(0); //aponta pro começo
+        raf.writeUTF(content); //escreve o conteúdo da string "content"
+
+        //vamos acrescentar um "final" pra verificar a funcionalidade do RandomAcessFile
+
+        raf.seek(raf.length()); //joga ponteiro pro final do texto
+
+        //agora vamos escrever "\n Fim do conteúdo! \n"
         
-        content.replaceAll("muito", "pouco");
+        raf.writeUTF("\n------------------\nFim do conteúdo! \n--------------------------");
 
-        try {
-            
-            String outputPathString = "/home/heitor/workspace/usp-2022.1/POO/Lista 3/Ex6Lista3/text2.txt";
-            Path outputPath = new Path.of(outputPathString);
-            //Files.writeString(outputPath, content, StandardCharsets.UTF_8);
-            
-            Files.writeString(outputPath, content, StandardCharsets.UTF_8);
 
-        } catch (Exception e) {
-            //TODO: handle exception
-            System.out.println("invalid path, bruh.");
+        //vamos pedir pra printar oq tem no arquivo, verificando se deu certo.
+        //aponta pro começo dnv: 
+        raf.seek(0);
+        //começa a printar o conteúdo do começo
+
+        while(raf.getFilePointer() < raf.length()){ // da posição do ponteiro até o final (tamanho do raf.)
+             System.out.println(raf.readUTF()); //read.UTF retorna uma String
         }
+
+        //fecha o RandomAcessFile
+        raf.close();
     }
 }
